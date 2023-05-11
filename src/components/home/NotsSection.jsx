0fdb779702953f0334e5c0b9963f7e41c4e91031
YeasarArefin <yeasararefin007@gmail.com>
2023-05-12 01:05:55 +0600

    fix : bug

--- src/components/home/NotsSection.jsx
+++ src/components/home/NotsSection.jsx
@@ -12,7 +12,6 @@ export default function NotsSection() {
    const [notes, setNotes] = useState([]);
 
    const { data, isLoading } = useSWR('http://localhost:3000/api/notes', fetcher);
-   console.log('🚀 ~ file: NotsSection.jsx:15 ~ NotsSection ~ isLoading:', isLoading);
 
    return (
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
