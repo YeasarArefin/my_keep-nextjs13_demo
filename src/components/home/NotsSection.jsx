67d8b4c07d674029d3ce8d159019f17a2e52638a
YeasarArefin <yeasararefin007@gmail.com>
2023-05-12 10:31:48 +0600

    api route changed

--- src/components/home/NotsSection.jsx
+++ src/components/home/NotsSection.jsx
@@ -11,7 +11,7 @@ import NoteCard from './NoteCard';
 export default function NotsSection() {
    const [notes, setNotes] = useState([]);
 
-   const { data, isLoading } = useSWR('http://localhost:3000/api/notes', fetcher);
+   const { data, isLoading } = useSWR('https://mykeeep.vercel.app/api/notes', fetcher);
 
    return (
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
